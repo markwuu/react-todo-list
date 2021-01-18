e956e98ef3299c120b4aed30cf81a0ee3767affb
Mark W <markwuu@gmail.com>
2021-01-17 18:01:52 -0800

    Added role to list for screen readers

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ function App(props) {
         3 tasks remaining
       </h2>
       <ul
+        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
